dc5ac029da0e2f3113b2c8c96d46ddd4d09e0217
jaechoi15 <jchoi625@gmail.com>
2018-04-25 00:53:24 -0400

    added bonus functions

--- Express/counter/server.js
+++ Express/counter/server.js
@@ -3,18 +3,38 @@ var bodyParser = require("body-parser");
 var session = require("express-session");
 var app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
 app.use(express.static(__dirname + "/static"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({secret: "codingdojorocks"}));
 
+// Increment counter by 1
 app.get("/", function (request, response) {
-    request.session.count = request.body.count
-    request.session.count += 1;
-    response.render("index", {count: count});
+    if (!request.session.counter) {
+        request.session.counter = 1;
+    } else {
+        request.session.counter++;
+    }
+    response.render("index", { counter: request.session.counter});
 })
 
-app.set("views", __dirname + "/views");
-app.set("view engine", "ejs");
+// Increment counter by 2
+app.post("/addTwo", function (request, response) {
+    if (!request.session.counter) {
+        request.session.counter = 1;
+    } else {
+        request.session.counter++;
+    }
+    response.redirect("/");
+})
+
+// Reset the counter back to 1
+app.post("/reset", function (request, response) {
+    request.session.destroy();
+    response.redirect("/");
+})
 
 app.listen(8000, function() {
     console.log("listening on port 8000")
